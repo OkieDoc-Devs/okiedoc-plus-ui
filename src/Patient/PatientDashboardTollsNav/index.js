a0c844b12579e5ffe16cb6d6669ec03090bf4180
Zaid B. Parvez <zaid.parvez@perpetual.edu.ph>
2025-09-04 20:59:14 +0800

    Revert "Merging of patient branch into main"

--- src/Patient/PatientDashboardTollsNav/index.js
+++
@@ -1,5 +0,0 @@
-export { default as PatientDashboard } from './Dashboard';
-export { default as MyAccount } from './MyAccount';
-export { default as ConsultationHistory } from './ConsultationHistory';
-
-

7b75ad4274c6cf32af149c3d736e19c2e4becec2
Zyron Niño L. Erandio <59166440+zyron9@users.noreply.github.com>
2025-09-19 10:40:39 +0800

    Update pdfHelpers.js

--- src/Admin/Specialistdashboard/ConsultationHistory/pdfHelpers.js
+++ src/Admin/Specialistdashboard/ConsultationHistory/pdfHelpers.js
@@ -1,1 +1,25 @@
+// Temporary placeholder functions for PDF/email actions
+export const downloadTreatmentPlanPDF = (consultation) => {
+  console.log("Download Treatment Plan PDF:", consultation);
+};
+
+export const downloadPrescriptionPDF = (consultation) => {
+  console.log("Download Prescription PDF:", consultation);
+};
+
+export const downloadLabRequestPDF = (consultation) => {
+  console.log("Download Lab Request PDF:", consultation);
+};
+
+export const downloadMedicalCertificatePDF = (consultation) => {
+  console.log("Download Medical Certificate PDF:", consultation);
+};
+
+export const downloadMasterPDF = (consultation) => {
+  console.log("Download Master PDF:", consultation);
+};
+
+export const sendToEmail = (consultation) => {
+  console.log("Send consultation to email:", consultation);
+};
 
